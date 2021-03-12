45bb61b44cb67944c8cf668d1c8ab66c19da69df
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2021-03-12 19:03:47 -0300

    Update gerar.sh

--- gerador/gerar.sh
+++ gerador/gerar.sh
@@ -642,6 +642,7 @@ fi
 bot_menu () {
 CIDdir=/etc/ADM-db && [[ ! -d ${CIDdir} ]] && mkdir ${CIDdir}
 [[ ! -e "${CIDdir}/confbot.sh" ]] && wget -O ${CIDdir}/confbot.sh https://raw.githubusercontent.com/rudi9999/TeleBotGen/master/confbot.sh &> /dev/null && chmod +x ${CIDdir}/confbot.sh
+sed -i -e 's/\r$//' ${CIDdir}/confbot.sh
 source ${CIDdir}/confbot.sh
 bot_conf
 }
